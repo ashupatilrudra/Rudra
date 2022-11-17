4ddb0da2f08f777a2dfcd3411c92ad61b819dbab
HP <HP@HP-PC>
2022-11-17 16:01:37 +0530

    by ashu

---
+++ Git_project/src/com/cjc/Demo1.java
@@ -0,0 +1,13 @@
+package com.cjc;
+
+public class Demo1
+{
+	public static void main(String[] args)
+	{
+		System.out.println("GitHub");
+	}
+public void m1()
+{
+	System.out.println("Simple method");
+}
+}
